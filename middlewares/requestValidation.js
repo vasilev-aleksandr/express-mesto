154bbba8c09de0d372ecd2da58c665d5426e4661
Aleksandr Vasilev <sovietxyouth@gmail.com>
2021-06-22 17:21:08 +0300

    fixed bugs

--- middlewares/requestValidation.js
+++ middlewares/requestValidation.js
@@ -43,9 +43,9 @@ const validateLogin = celebrate({
 
 const validateAddUser = celebrate({
   body: Joi.object().keys({
-    name: Joi.string().required().min(2).max(30),
-    about: Joi.string().required().min(2).max(20),
-    avatar: Joi.string().custom(urlValidation).required(),
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(20),
+    avatar: Joi.string().custom(urlValidation),
     email: Joi.string().required().email(),
     password: Joi.string().required().min(8),
   }),
